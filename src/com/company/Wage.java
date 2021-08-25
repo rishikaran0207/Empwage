0eeb2be96b3aa568abda18f56bcac1df069feacd
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-25 21:06:16 +0530

    Added: SwitchCase

--- src/com/company/Wage.java
+++ src/com/company/Wage.java
@@ -1,19 +1,40 @@
-package com.company;
+q
 
-        public class Wage {
-            public static void main(String[] args){
-                int Full_Time_Emp = 1;
-                int empcheck = (int) Math.floor(Math.random() * 10 % 2);
-                System.out.println(empcheck);
-                if(empcheck == Full_Time_Emp)
+public class Wage {
 
-                {
-                    System.out.println("The Emp is Present");
-                } else
+    public static final int Full_Time_Emp = 1;
+    public static final int Part_Time =2;
+    public static final int Emp_Rate_per_hour = 20;
+    public static final int Num_Of_Working_days = 20;
 
-                {
-                    System.out.println("The Emp is Absent");
-                }
+    public static void main(String[] args){
+
+
+        int empHrs = 0;
+        int total_Wage = 0;
+
+        for (int day = 1; day <= Num_Of_Working_days; day++) {
+            double empcheck = Math.floor(Math.random() * 10) % 3;
+
+            switch ((int) empcheck) {
+                case Full_Time_Emp:
+                    empHrs = 8;
+                    break;
+                case Part_Time:
+                    empHrs = 4;
+                    break;
+
+                default:
+                    empHrs = 0;
             }
+            int empwage;
+            empwage = empHrs * Emp_Rate_per_hour;
+            total_Wage = total_Wage + empwage;
+            System.out.println("day" + day);
+            System.out.println("Employee Wage Is " + empwage);
         }
+        System.out.println("Total Wage:"+total_Wage);
+    }
+}
+
 
