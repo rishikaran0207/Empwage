57e01a8601894812e73c7262758780879dced6ef
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-25 22:04:29 +0530

    Added: Calc100Hrs

--- src/com/company/Wage.java
+++ src/com/company/Wage.java
@@ -1,19 +1,21 @@
 
-
 public class Wage {
 
     public static final int Full_Time_Emp = 1;
-    public static final int Part_Time =2;
+    public static final int Part_Time = 2;
     public static final int Emp_Rate_per_hour = 20;
     public static final int Num_Of_Working_days = 20;
+    public static final int MAX_HRS_PER_MONTH = 100;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
         int empHrs = 0;
         int total_Wage = 0;
+        int totalHrs = 0;
 
-        for (int day = 1; day <= Num_Of_Working_days; day++) {
+        int day = 1;
+        while (day <= Num_Of_Working_days && totalHrs < MAX_HRS_PER_MONTH) {
             double empcheck = Math.floor(Math.random() * 10) % 3;
 
             switch ((int) empcheck) {
@@ -29,10 +31,18 @@ public class Wage {
             }
             int empwage;
             empwage = empHrs * Emp_Rate_per_hour;
-            total_Wage = total_Wage + empwage;
+
+            totalHrs += empHrs;
+            total_Wage += empwage;
+
             System.out.println("day" + day);
             System.out.println("Employee Wage Is " + empwage);
-        }
+            day++;
+
+
+    }
+
+            System.out.println("Total Hrs:" + totalHrs);
         System.out.println("Total Wage:"+total_Wage);
     }
 }
